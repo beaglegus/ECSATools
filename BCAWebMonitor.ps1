901bfd3ff722a876c8f7c9e07db35130bed14b3e
beaglegus <zerodefiance@yahoo.com>
2018-06-27 21:51:36 -0400

    Update BCAWebMonitor.ps1

--- BCAWebMonitor.ps1
+++ BCAWebMonitor.ps1
@@ -3,12 +3,12 @@
 
 $UsersFile = "C:\Epic\Data\Epic BCA Client\_users_.emp"
 $MaxDays = 3
-$NotificationEmail = "adam.buchanan@midmichigan.org", "bob.mccomb@midmichigan.org"
+$NotificationEmail = "arb@company.org", "ar2b@company.org"
 
 
 If (Test-Path $UsersFile -OlderThan (Get-Date).AddDays(-$MaxDays)) 
 {
     
-    send-mailmessage -to $NotificationEmail -from "BCAWebMonitorScript@midmichigan.org" -subject "BCAWeb Server - Users file not getting updated." -Body "Server: $env:computername.   The _users_.emp file is older than $MaxDays days.  Could mean BCA Web server is in edit mode or that users batch is not running.  Please fix.  Thanks" -SmtpServer smtp.midmichigan.net
+    send-mailmessage -to $NotificationEmail -from "BCAWebMonitorScript@company.org" -subject "BCAWeb Server - Users file not getting updated." -Body "Server: $env:computername.   The _users_.emp file is older than $MaxDays days.  Could mean BCA Web server is in edit mode or that users batch is not running.  Please fix.  Thanks" -SmtpServer smtp.company.net
 
 }
